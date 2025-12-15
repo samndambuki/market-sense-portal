0bcd183c2220db3d2fa4fe4cda98825039f7d736
samndambuki <sam.ndambuki08@gmail.com>
2025-12-15 15:59:59 +0300

    working on market service

--- src/app/features/market-explorer/services/market.service.ts
+++ src/app/features/market-explorer/services/market.service.ts
@@ -1,11 +1,34 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, signal } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Market } from '../models/market.model';
+import { API_ENDPOINTS } from '../../../core/config/api-endpoints';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MarketService {
   constructor(private http: HttpClient) {}
-  readonly markets = signal<Market[]>([]);
+  markets: Market[] = [];
+  loading: boolean = false;
+  error: boolean | null = null;
+
+  loadMarkets(): void {
+    this.loading = true;
+    this.error = null;
+
+     this.http.get<Market[]>(API_ENDPOINTS.markets).subscribe({
+      next: (data) => {
+        this.markets = data;
+        this.loading = false;
+      },
+      error: () => {
+        this.error = true;
+        this.loading = false;
+      },
+    });
+  }
+
+  getMarket(id: number) {
+    return this.http.get<Market>(`${API_ENDPOINTS.markets}/${id}`);
+  }
 }
