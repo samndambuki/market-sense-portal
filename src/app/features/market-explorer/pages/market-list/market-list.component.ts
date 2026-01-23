fb14b3871d29354f26036520617467715587bd89
samndambuki <sam.ndambuki08@gmail.com>
2026-01-23 09:28:15 +0300

    working on search logic for markets

--- src/app/features/market-explorer/pages/market-list/market-list.component.ts
+++ src/app/features/market-explorer/pages/market-list/market-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MarketService } from '../../services/market.service';
 import { CommonModule } from '@angular/common';
+import { Market } from '../../models/market.model';
 
 @Component({
   selector: 'app-market-list',
@@ -9,8 +10,45 @@ import { CommonModule } from '@angular/common';
   styleUrl: './market-list.component.scss',
 })
 export class MarketListComponent implements OnInit {
+  searchTerm = '';
+  selectedCategory = 'All';
+  selectedRegion = 'All';
+  selectedGrowth = 'All';
+
+  filteredMarkets: Market[] = [];
+
   constructor(public marketService: MarketService) {}
   ngOnInit(): void {
     this.marketService.loadMarkets();
+    setTimeout(() => {
+      this.filteredMarkets = this.marketService.markets;
+    }, 0);
+  }
+
+  applyFilters() {
+    this.filteredMarkets = this.marketService.markets.filter((market) => {
+      const matchesSearch = market.name
+        .toLowerCase()
+        .includes(this.searchTerm.toLowerCase());
+
+      const matchesCategory =
+        this.selectedCategory === 'All' ||
+        market.category === this.selectedCategory;
+
+      const matchesRegion =
+        this.selectedRegion === 'All' || market.region === this.selectedRegion;
+
+      const growthLevel =
+        market.growthRate >= 15
+          ? 'High'
+          : market.growthRate >= 10
+          ? 'Medium '
+          : 'Low';
+
+      const matchesGrowth =
+        this.selectedGrowth === 'All' || growthLevel === this.selectedGrowth;
+
+      return matchesSearch && matchesCategory && matchesRegion && matchesGrowth;
+    });
   }
 }
