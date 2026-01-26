589c7153ab0083d7e2e4a144e329d251f58d587c
samndambuki <sam.ndambuki08@gmail.com>
2026-01-26 07:55:27 +0300

    worked on search and filter markets

--- src/app/features/market-explorer/pages/market-list/market-list.component.ts
+++ src/app/features/market-explorer/pages/market-list/market-list.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { MarketService } from '../../services/market.service';
 import { CommonModule } from '@angular/common';
 import { Market } from '../../models/market.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-market-list',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './market-list.component.html',
   styleUrl: './market-list.component.scss',
 })
@@ -42,8 +43,8 @@ export class MarketListComponent implements OnInit {
         market.growthRate >= 15
           ? 'High'
           : market.growthRate >= 10
-          ? 'Medium '
-          : 'Low';
+            ? 'Medium'
+            : 'Low';
 
       const matchesGrowth =
         this.selectedGrowth === 'All' || growthLevel === this.selectedGrowth;
