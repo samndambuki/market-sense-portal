3a0ae88df3f477d5d6d2671631d5d66bcc05fd42
samndambuki <sam.ndambuki08@gmail.com>
2026-06-09 18:21:38 +0300

    updates before pulling

--- src/app/features/competitor-analysis/competitor-analysis.module.ts
+++ src/app/features/competitor-analysis/competitor-analysis.module.ts
@@ -5,4 +5,6 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [CommonModule],
 })
-export class CompetitorAnalysisModule {}
+export class CompetitorAnalysisModule {
+
+}
