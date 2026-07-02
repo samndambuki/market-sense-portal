2e82810e44884aa09ad645323f20c0cc6a0217ae
samndambuki <sam.ndambuki08@gmail.com>
2026-07-02 10:58:03 +0300

    working on login

--- src/app/features/auth/pages/login/login.component.ts
+++ src/app/features/auth/pages/login/login.component.ts
@@ -1,13 +1,62 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { Component, inject, Inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormsModule,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule,RouterModule],
+  imports: [FormsModule, RouterModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  private fb = inject(FormBuilder);
+  constructor(
+    private userService: UserService,
+    private router: Router,
+  ) {}
 
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  });
+
+  login() {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    const email = this.loginForm.value.email!;
+    const password = this.loginForm.value.password;
+
+    this.userService.login(email).subscribe({
+      next: (users) => {
+        if (users.length === 0) {
+          alert('Invalid email or password');
+          return;
+        }
+
+        const user = users[0];
+
+        if (user.password !== password) {
+          alert('Invalid email or password');
+          return;
+        }
+
+        alert('login successful');
+
+        this.router.navigate(['/markets']);
+      },
+      error: (error) => {
+        console.error(error);
+        alert(error);
+      },
+    });
+  }
 }
--- src/app/features/auth/services/user.service.ts
+++ src/app/features/auth/services/user.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root',
@@ -8,4 +9,7 @@ import { environment } from '../../../../environments/environment';
 export class UserService {
   private userapi = environment.api.users;
   constructor(private http: HttpClient) {}
+  login(email: string) {
+    return this.http.get<User[]>(`${this.userapi}?email=${email}`);
+  }
 }
